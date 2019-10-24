552d3824651cbc7515d15c7bb067e86f173e2303
sgbbhat <sgbbhat@gmail.com>
2019-10-24 14:04:35 -0500

    Modifed op

--- Development/Tests/Vin_Voltage.py
+++ Development/Tests/Vin_Voltage.py
@@ -4,13 +4,16 @@ from os import *
 import time 
 import re
 from Tests.displayResult import displayResult
+from tkinter import *
+from tkinter import messagebox
+
 
 def Vin_Voltage(root, key, val, databaseHandle, mfgID, Sln, TestNameText, MinLimitText, MaxLimitText, MeasurementText, ResultText, modelFileContent, testStartTime, OperationMode, OperationModeInput, LotNumvberInput):
 	rawScale = popen('megaioind 0 ruin 3').read()
 	measurement = float(rawScale)
 
 	if measurement < float(val[1]) or measurement > float(val[2]) :
-		messagebox.showerror("Error", "Check if the bridge is Soldering - \n Solder the bridge and press OK")
+		messagebox.showerror("Error", "Check if the bridge is Soldered \n \nSolder the bridge and press OK")
 		rawScale = popen('megaioind 0 ruin 3').read()
 		measurement = float(rawScale)
 
